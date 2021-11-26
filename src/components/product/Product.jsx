66f542b275be94fc509f93d0e73dd2e412538a57
F3NRN4ND02 <fernando.ujap@gmail.com>
2021-11-26 17:04:32 -0400

    some_improvements

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,6 +1,6 @@
 import "./product.css";
 
-const Product = ({img,link}) => {
+const Product = ({img,link,name,created,about,pushed}) => {
   return (
     <div className="p">
       <div className="p-browser">
@@ -9,6 +9,10 @@ const Product = ({img,link}) => {
         <div className="p-circle"></div>
       </div>
       <a href={link} target="_blank" rel="noreferrer">
+        <h3 className='title'>{name}</h3>
+        <h3 className='title'>{created}</h3>
+        <p className='p-about'>{about}</p>
+        <p className='p-pushed'>{pushed}</p>
         <img src={img} alt="" className="p-img" />
       </a>
     </div>
--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,12 +1,33 @@
+import React, {Component} from "react";
 import "./productList.css";
 import Product from "../product/Product";
 import { products } from "../../data";
+import axios from "axios";
 
-const ProductList = () => {
-  return (
-    <div className="pl">
+const api = axios.create({
+  baseURL: 'https://api.github.com/users/F3RN4ND02/repos'
+})
+
+
+class Productss extends Component {
+  
+  state = {
+    rep: []
+  }
+
+  constructor() {
+    super();
+    axios.get('https://api.github.com/users/F3RN4ND02/repos').then(res => {
+      console.log(res.data.slice(-6))
+      this.setState({ rep:res.data.slice(-6)})
+    })
+  }
+  
+  render() {
+    return (
+      <div className="pl">
       <div className="pl-texts">
-        <h1 className="pl-title">Create & inspire. It's Lama</h1>
+        <h1 className="pl-title">You can go to my github profile or get my latest 6 repos</h1>
         <p className="pl-desc">
           Lama is a creative portfolio that your work has been waiting for.
           Beautiful homes, stunning portfolio styles & a whole lot more awaits
@@ -14,12 +35,34 @@ const ProductList = () => {
         </p>
       </div>
       <div className="pl-list">
-        {products.map((item) => (
-          <Product key={item.id} img={item.img} link={item.link} />
+        {this.state.rep.map(rep => <Product key={rep.id} link={rep.html_url} name={rep.name} created={rep.created_at} about={rep.description} pushed={rep.pushed_at} />)}
+      {products.map((item) => (
+          <Product key={item.id} img={item.img} link={item.link}  />
         ))}
       </div>
     </div>
-  );
-};
+    )
+  }
+    
+}
 
-export default ProductList;
+export default Productss
+/*
+async getData() {
+/*axios.get('https://api.github.com/users/F3RN4ND02/repos')
+.then(res => {
+  console.log(res.data)
+  return res.data
+})
+var rep
+axios({
+  method: 'get',
+  url: 'https://api.github.com/users/F3RN4ND02/repos',
+})
+  /*.then(function (response) { 
+    rep = response.data.slice(-6)
+    console.log(rep)
+    rep.forEach(element => console.log(element.name)
+  );
+  });*/
+
